c71dc148d9192afa1d0d10c04d8728d31964205b
viwnu <xenmysafitiplace@gmail.com>
2023-03-29 14:10:13 +0300

    preapre root react index.js and app.js files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import Hello from "./hello";
+import ReactDOM from 'react-dom/client';
+import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<Hello />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
